16f0122643928376a18f5f43ca05c2e7625240fe
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-04 15:36:36 +0000

    create endpoint to create new account

--- router/accountRouter.js
+++ router/accountRouter.js
@@ -14,4 +14,42 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const account = await db('accounts').where({ id: req.params.id });
+        res.json(account[0])
+    } catch (error) {
+        res.status(500).json({
+            message: "Account not Found" + error.message
+        })
+    }
+})
+
+router.post('/', async(req, res) => {
+    try {
+        const account = await db('accounts')
+            .insert({
+                name: req.body.name,
+                budget: req.body.budget
+            })
+        res.status(200).json('New account created has an id of ' + account)
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating account" + error.message
+        })
+    }
+})
+
+// function validateId (req, res, next) {
+//     const { id } = req.params.id;
+
+//     if(req.params.id) {
+
+//     } else {
+//         res.json({
+//             message: "Account not found"
+//         })
+//     }
+// }
+
 module.exports = router;
